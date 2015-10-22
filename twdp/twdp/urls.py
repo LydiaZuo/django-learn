e30565a75979738d27d825e63784521e851a458b
Su Chen <s0en1it@SudeMacBook-Pro.local>
2015-10-22 23:16:52 +0800

    5.4 not finished

--- twdp/twdp/urls.py
+++ twdp/twdp/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^rango/', include('rango.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += url(r'^media/(?P<path>.*)', 'django.views.static')
+'''    urlpatterns += patterns(
+        'django.views.static',
+        (r'^media/(?P<path>.*)',
+        'serve',
+        {'document_root': settings.MEDIA_ROOT}), )
+'''
