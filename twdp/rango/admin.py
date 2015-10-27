80cbe1b39688ac90f4300c252c216aa4432b5cb7
Su Chen <s0en1it@SudeMacBook-Pro.local>
2015-10-28 00:31:21 +0800

    tutorial 2

--- twdp/rango/admin.py
+++ twdp/rango/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
+
 # Register your models here.
-admin.site.register(Category)
-admin.site.register(Page)
+
+class CategoryAdmin(admin.ModelAdmin):
+	fieldsets = [
+		(None, {'fields' : ['name', ]}),
+		('Detail information', {'fields' : ['views', 'likes']}),
+		]
+
+class PageAdmin(admin.ModelAdmin):
+	fieldsets = [
+		(None, {'fields' : ['category', 'title']}),
+		('Detail information', {'fields' : ['url', 'views', ]}),
+		]
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)
